f914871bd96fa6bb538ff86c48f8e249d1465d54
miketorii <miketorii@gmail.com>
2024-03-28 20:03:50 +0900

    update struct

--- struct_program/src/main.rs
+++ struct_program/src/main.rs
@@ -1,3 +1,6 @@
+/////////////////////////////////////////////////////
+// User
+//
 struct User {
     username: String,
     email: String,
@@ -5,8 +8,44 @@ struct User {
     active: bool,
 }
 
+fn build_user(email: String, username: String) -> User{
+    User {
+        username: username,
+        email: email,
+        sign_in_count: 1,
+        active: true,
+    }
+}
+
+fn print_user(user: User){
+    println!("{}",user.username);
+    println!("{}",user.email);
+    println!("{}",user.sign_in_count);
+    println!("{}",user.active);
+}
+
+/////////////////////////////////////////////////////
+// Rectangle
+//
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+impl Rectangle {
+    fn area10(&self) -> u32 {
+        self.width * self.height * 10
+    }
+}
+/////////////////////////////////////////////////////
+//
+//
 fn main() {
-    println!("---struct---");
+    println!("---struct User---");
 
     let user1 = User {
         username: String::from("MikeT"),
@@ -15,8 +54,13 @@ fn main() {
         active: true,
     };
 
-    println!("{}",user1.username);
-    println!("{}",user1.email);
-    println!("{}",user1.sign_in_count);
-    println!("{}",user1.active);
+    print_user(user1);
+
+    let user2 = build_user("Tom".to_string(), "tom@gmail.com".to_string());
+    print_user(user2);
+
+    println!("---struct Rectangle---");
+    let rect = Rectangle { width: 20, height: 10 };
+    println!("area = {}", area(&rect));
+    println!("area10 method way = {}", rect.area10());
 }
