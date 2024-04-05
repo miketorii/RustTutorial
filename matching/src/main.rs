4013af7c4df7851654439acdce6e419494294dea
miketorii <miketorii@gmail.com>
2024-04-06 08:40:27 +0900

    add Some

--- matching/src/main.rs
+++ matching/src/main.rs
@@ -14,4 +14,12 @@ fn main() {
         IpAddrKind::V6 => {println!("v6");},
     }
 
+    println!("-----Option Some/None-----");
+
+    let some_number = Some(5);
+    println!("Some num={:?}", some_number);
+    let some_string = Some("string in Some");
+    println!("Some num={:?}", some_string);
+    let absent_number : Option<i32> = None;
+    println!("None num={:?}", absent_number);
 }
